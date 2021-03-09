55f76a9b4af0cdfb7dabafbdb80247446c684304
Eugene Kubesh <codbasters@gmail.com>
2021-03-09 11:20:21 +0300

    TU-T2437 add examples of multiline using

--- playground/app/components/vertical-multiline/vertical-multiline.component.ts
+++ playground/app/components/vertical-multiline/vertical-multiline.component.ts
@@ -2,20 +2,16 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'vertical-multiline',
-  templateUrl: 'vertical-multiline.component.html'
+  templateUrl: 'vertical-multiline.component.html',
+  styleUrls: [
+    './vertical-multiline.component.scss',
+  ],
 })
 export class VerticalMultilineComponent implements OnInit {
 
-  selected = null;
-
-  items = [
-      { name: 'Item 1', subname: 'Sub Name 1', id: 1 },
-      { name: 'Item 2', subname: 'Sub Name 2', id: 2 },
-      { name: 'Item 3', subname: 'Sub Name 3', id: 3 },
-      { name: 'Item 4', subname: 'Sub Name 4', id: 4 }
-    ];
+  public selected = null;
+  public value: number;
 
   ngOnInit() {
-    this.selected = this.items[1];
   }
 }
