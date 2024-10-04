962562076dd8af6d2a506ce9c82bc297b5469f4d
Raymond Gigliotti <ray@firestitch.com>
2024-10-04 04:05:14 -0400

    fixed demo site

--- playground/app/playground.module.ts
+++ playground/app/playground.module.ts
@@ -1,41 +1,39 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { FsExampleModule } from '@firestitch/example';
 import { FsFormModule } from '@firestitch/form';
 import { FsMessageModule } from '@firestitch/message';
 import { FsRadioGroupModule } from '@firestitch/radiogroup';
 
-import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { AppMaterialModule } from './material.module';
-import { VerticalComponent } from './components/vertical/vertical.component';
 import { HorizontalComponent } from './components/horizontal/horizontal.component';
 import { VerticalMultilineComponent } from './components/vertical-multiline/vertical-multiline.component';
+import { VerticalComponent } from './components/vertical/vertical.component';
+import { AppMaterialModule } from './material.module';
 
 
 @NgModule({
-    bootstrap: [AppComponent],
-    imports: [
-        BrowserModule,
-        FsRadioGroupModule,
-        BrowserAnimationsModule,
-        AppMaterialModule,
-        FormsModule,
-        FsFormModule,
-        FsExampleModule.forRoot(),
-        FsMessageModule.forRoot(),
-        ToastrModule.forRoot({ preventDuplicates: true }),
-    ],
-    declarations: [
-        AppComponent,
-        HorizontalComponent,
-        VerticalComponent,
-        VerticalMultilineComponent,
-    ]
+  bootstrap: [AppComponent],
+  imports: [
+    BrowserModule,
+    FsRadioGroupModule,
+    BrowserAnimationsModule,
+    AppMaterialModule,
+    FormsModule,
+    FsFormModule,
+    FsExampleModule.forRoot(),
+    FsMessageModule.forRoot(),
+  ],
+  declarations: [
+    AppComponent,
+    HorizontalComponent,
+    VerticalComponent,
+    VerticalMultilineComponent,
+  ],
 })
 export class PlaygroundModule {
 }
