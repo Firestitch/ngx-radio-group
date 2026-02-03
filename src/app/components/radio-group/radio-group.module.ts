d99e7819712bd91e5734cb4110ba643d20ace74b
Raymond Gigliotti <ray@firestitch.com>
2026-02-03 06:49:03 -0500

    Update package version to 18.0.10, add FsRadioGroupModule for improved modularity, and export it in public API.

---
+++ src/app/components/radio-group/radio-group.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+
+import { FsRadioGroupComponent } from './radio-group.component';
+
+@NgModule({
+  imports: [FsRadioGroupComponent],
+  exports: [FsRadioGroupComponent],
+})
+export class FsRadioGroupModule {}
--- src/public_api.ts
+++ src/public_api.ts
@@ -1,3 +1,3 @@
-
-
 export { FsRadioGroupComponent } from './app/components/radio-group/radio-group.component';
+export { FsRadioGroupModule } from './app/components/radio-group/radio-group.module';
+
